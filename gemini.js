a78642e5f6ddc7d4fd512469594b0a6ce87412ef
Alan De Luna <alandeluna18@gmail.com>
2025-04-05 20:22:24 -0700

    Added Javascript Function That Uses Google Gemini API

---
+++ gemini.js
@@ -0,0 +1,22 @@
+import { GoogleGenAI } from "@google/genai";
+import { get } from "http";
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAx_SFrDQJItOQV2NKwqKz-Rxz7yLL_p5A" });
+
+
+async function getZipcode(preferences, zipcode) {
+    try {
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: "Find the best " + preferences + " at this city: " + zipcode,
+        });
+        
+        console.log(response.text); // Ensure this logs the resolved response
+    } catch (error) {
+        console.error("Error generating content:", error);
+    }
+}
+
+const testa = "gyms";
+const testb = "Oceanside, CA";
+getZipcode(testa, testb);
